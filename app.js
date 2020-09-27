8515e0d843f92d0fc337233d540f0d8ef7381882
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-09-26 21:28:36 -0700

    init views

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/users");
 
 const app = express();
+
+// EJS Middleware
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
 // Routes
 app.use("/", indexRouter);
