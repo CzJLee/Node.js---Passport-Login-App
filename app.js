f923f62d1f079def91ca6bc62e27238340b3297a
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-09-27 13:31:06 -0700

    correct typo

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose
 	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 	// (node:18781) DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
 	.then(console.log("MongoDB Connected."))
-	.catch((Error) => console.log(error));
+	.catch((error) => console.log(error));
 
 const app = express();
 
