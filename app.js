d10198c5ff5497174244ebc12273041e80e63c1e
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-09-26 21:26:46 -0700

    Setup basic routes

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require("express")
+const express = require("express");
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/users");
 
 const app = express();
 
+// Routes
+app.use("/", indexRouter);
+app.use("/users", userRouter);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
