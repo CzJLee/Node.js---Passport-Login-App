d8ebc6a7e919d6413e48b5f855bd6ff7a83ce937
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-09-26 22:23:12 -0700

    Handle register post requests and error check

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,43 @@ router.get("/register", (req, res, next) => {
 	res.render("register");
 });
 
+// Register
+// Handle Registration Post Requests
+router.post("/register", (req, res, next) => {
+	console.log(req.body);
+
+	// Object destructuring to create variables from the object.
+	const { name, email, password, password2 } = req.body;
+
+	// Error checking
+	let errors = [];
+
+	// Check required fields
+	// If a field is empty
+	if (!name || !email || !password || !password2) {
+		// Then add a message object to the list of errors
+		errors.push({ message: "Please fill in all fields" });
+	}
+
+	// Verify that passwords match
+	if (password !== password2) {
+		errors.push({ message: "Passwords do not match" });
+	}
+
+	// Verify that password length is >= 6
+	if (password.length < 6) {
+		errors.push({ message: "Password must be at least 6 characters" });
+	}
+
+	// Check if any errors occurred
+	if (errors.length > 0) {
+		// An error occurred
+		res.render("register", { errors, name, email, password, password2 });
+		// When using a render engine, we can pass in values
+		// Pass the values, and then pass the previously entered values so that the form does not clear
+	} else {
+		res.send("pass");
+	}
+});
+
 module.exports = router;
